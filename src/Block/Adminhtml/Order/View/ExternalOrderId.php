541de4667d869e6243a2308771269533c61919fe
Alek Mosingiewicz <almos666@gmail.com>
2019-08-22 21:06:37 +0200

    Show external order id on admin.

---
+++ src/Block/Adminhtml/Order/View/ExternalOrderId.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AlekMosingiewicz\ExternalOrderId\Block\Adminhtml\Order\View;
+
+use Magento\Framework\Registry;
+use Magento\Framework\View\Element\Template\Context as TemplateContext;
+use Magento\Payment\Helper\Data as PaymentHelper;
+use Magento\Sales\Model\Order\Address\Renderer as AddressRenderer;
+
+class ExternalOrderId extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * @var Registry
+     */
+    private $_registry;
+
+    protected $_template = 'AlekMosingiewicz_ExternalOrderId::order/view/external_order_id.phtml';
+
+    /**
+     * ExternalOrderId constructor.
+     * @param Registry $registry
+     */
+    public function __construct(        TemplateContext $context,
+                                        Registry $registry,
+                                        array $data = [])
+    {
+        $this->_registry = $registry;
+        parent::__construct($context, $data);
+    }
+
+
+    public function setTemplate($template)
+    {
+        return parent::setTemplate('AlekMosingiewicz_ExternalOrderId::order/view/external_order_id.phtml');
+    }
+
+    public function getOrder()
+    {
+        return $this->_registry->registry('current_order');
+    }
+}
