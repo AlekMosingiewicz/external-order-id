7fc288e271609f423807df2272e45565e569f2e5
Alek Mosingiewicz <almos666@gmail.com>
2019-08-23 18:35:31 +0200

    Fix bad refactor.

--- src/Block/Adminhtml/Order/View/ExternalOrderId.php
+++ src/Block/Adminhtml/Order/View/ExternalOrderId.php
@@ -7,10 +7,5 @@ use Magento\Framework\View\Element\Template\Context as TemplateContext;
 
 class ExternalOrderId extends \AlekMosingiewicz\ExternalOrderId\Block\Common\ExternalOrderId
 {
-    /**
-     * @var Registry
-     */
-    private $_registry;
-
     protected $_template = 'AlekMosingiewicz_ExternalOrderId::order/view/external_order_id.phtml';
 }
